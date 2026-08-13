ecd87712f0593af1cd155ac1beee8e36e4b3ff8c
Dean Galvin <deangalvin3@gmail.com>
2026-08-13 16:32:55 -0400

    Fix cors (#489)

--- internal/http/middleware/httpcors.go
+++ internal/http/middleware/httpcors.go
@@ -8,7 +8,7 @@ import (
 
 func HttpCORS(next http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowedOrigins:   []string{"http://localhost:8080", "https://gojellyfin.deangalvin.dev"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
